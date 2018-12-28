3699e6102dec0494137bad86016ef199bc13817f
szajberpiraty <szajberpiraty@gmail.com>
2018-12-28 14:18:31 +0100

    sima post megvalósítása

--- ToDoApp/Controllers/TodoController.cs
+++ ToDoApp/Controllers/TodoController.cs
@@ -9,24 +9,26 @@ namespace ToDoApp.Controllers
 {
     public class TodoController:Controller
     {
+       
+
+
         public ActionResult Index()
         {
-            var lista = new List<TodoItem>();
-
-            lista.Add(new TodoItem() { Name = "Só", Done = true });
-            lista.Add(new TodoItem() { Name = "Cukor", Done = true });
-            lista.Add(new TodoItem() { Name = "Spagetti", Done = true });
-            lista.Add(new TodoItem() { Name = "Marhahús", Done = false });
-            lista.Add(new TodoItem() { Name = "Paradicsom", Done = false });
+                                   
 
             //ViewBag.lista = lista;
 
-            return View(lista);
+            return View(MyDb.Lista);
         }
 
-        public ActionResult Create()
+        public ActionResult Create(string Name)
         {
 
+            if (!string.IsNullOrEmpty(Name))
+            {
+                MyDb.Lista.Add(new TodoItem() { Name = Name, Done = true });
+                return RedirectToAction("Index");
+            }
 
 
             return View();
---
+++ ToDoApp/Models/MyDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApp.Models
+{
+    public class MyDb
+    {
+
+        //Ideiglenes adatbázis, csak teszt célokra
+        public static List<TodoItem> Lista = new List<TodoItem>()
+        {
+            new TodoItem() { Name = "Só", Done = true },
+            new TodoItem() { Name = "Cukor", Done = true },
+            new TodoItem() { Name = "Spagetti", Done = true },
+            new TodoItem() { Name = "Marhahús", Done = false },
+            new TodoItem() { Name = "Paradicsom", Done = false }
+
+        };
+
+
+    }
+}
